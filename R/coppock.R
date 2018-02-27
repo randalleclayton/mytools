8e7701f43488879aec8bd5f19b086ef9b386725a
Randy Clayton <randyclayto@gmail.com>
2018-02-27 14:24:25 +0000

    Cleanup Comments

--- R/coppock.R
+++ R/coppock.R
@@ -1,28 +1,28 @@
 #
-# Coppock Index for a price time series. Mostly used with stocks and ETFs
-# using end of month price data. A buy signal is generated when the index
-# rises from a negative value. Generates buy signals only, there is no sell
-# signal. Works very well with indexes (i.e., S&P 500) and some large cap
-# stocks. Less reliable on single stocks, better on indexes or ETFs with
-# larger number of stocks.
+# Coppock Index is the weighted moving average of the sum of the 11
+# and 14 month rate of change of a monthly price series.
 #
-# The weighted moving average of the 11 and 14 month Rate Of Change.
+# Coppock Index provides buy signals only. A buy signal is generated
+# when the index rises from a negative value.
+#
+# Works very well with indexes (i.e., S&P 500) and some large cap equities.
+#
+# Not useful on small cap stocks.
 #
 
 coppock <- function(symbol, start_date = '2000-01-01',
                     end_date = Sys.Date()) {
 
   # For Yahoo quotes ... BRK.B => BRK-B
-  # Quandl uses _
-  # symbol <- gsub("\\.", "-", symbol)
-  # Get prices, adjust to monthly close, compute ROC11 and ROC14
+  symbol <- gsub("\\.", "-", symbol)
+
   q <- tq_get(symbol, from = start_date) %>%
     tq_transmute_xy(x = adjusted, mutate_fun = to.period,
                     period = "months") %>%
     tq_mutate(select = adjusted, mutate_fun = ROC, n = 11) %>%
     tq_mutate(select = adjusted, mutate_fun = ROC, n = 14)
 
-  # Coppock Index == WMS(ROC11+ROC14)
+  # Coppock Index == WMA(ROC11+ROC14)
   s <- tibble::add_column(q, "sum" = (q[,'ROC'] + q[,'ROC.1'])[,1],
                           .before = 3)
   cop <- round(TTR::WMA(s[,"sum"], n = 10),2)
